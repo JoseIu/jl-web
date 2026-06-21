990a17f2d2aef5608ee12aea2d7597a078f3ef2d
Joselu <boxixz0991@gmail.com>
2026-06-21 12:39:26 +0200

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jl-web-xi.vercel.app",
   integrations: [sitemap()],
+  adapter: vercel(),
   // output: "server",
 });
